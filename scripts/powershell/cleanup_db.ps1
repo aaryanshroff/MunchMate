33ddadf59a8394bea1e7ec281874337b58da7329
Diyar Yasin <diyaryasin.dy@gmail.com>
2025-03-08 17:21:29 -0500

    added powershell support - untested

--- scripts/powershell/cleanup_db.ps1
+++ scripts/powershell/cleanup_db.ps1
@@ -1,31 +1,35 @@
+# Set error action to stop execution on errors
+$ErrorActionPreference = "Stop"
+
+# Save current directory so we can jump back here after this script finishes
+Push-Location
+
+# All paths relative to our backend directory
+Set-Location -Path "..\..\backend"
+
 # Environment Variables
 #
 # DB_TYPE: 
 #   desc:        use production database or sample?
 #   valid input: { "sample", "prod" }
 #   defaults:    "sample"
-
-# Set DB_TYPE from environment variable or default to "sample"
 $DB_TYPE = $env:DB_TYPE
 if (-not $DB_TYPE) { $DB_TYPE = "sample" }
 
-# Database filepaths
-$MAIN_DB_DIR = "..\..\backend\databases"
-$SAMPLE_DB_DIR = "$MAIN_DB_DIR\sample_db"
-$PROD_DB_DIR = "$MAIN_DB_DIR\prod_db"
-
-$SAMPLE_DB_FILEPATH = "$SAMPLE_DB_DIR\sample_dataset.db"
-$PROD_DB_FILEPATH = "$PROD_DB_DIR\prod_dataset.db"
-
-$DB_FILEPATH = ""
-
-if ($DB_TYPE -eq "sample") {
-    Write-Host "Cleaning sample database..."
-    $DB_FILEPATH = $SAMPLE_DB_FILEPATH
-} else {
-    Write-Host "Cleaning production database..."
-    $DB_FILEPATH = $PROD_DB_FILEPATH
+if ($DB_TYPE -ne "sample" -and $DB_TYPE -ne "prod") {
+    Write-Host "ERROR: Env var DB_TYPE expects value sample or prod. Given $DB_TYPE"
+    Write-Host "Exiting early..."
+    Pop-Location
+    exit 1
 }
+
+Write-Host "Using $DB_TYPE database."
+
+# Setup database filepaths
+$DB_DIR = "databases"
+$DB_FILEPATH = "$DB_DIR\$DB_TYPE`_db\$DB_TYPE`_dataset.db"
+
+Write-Host "Cleaning $DB_TYPE database..."
 
 if (Test-Path $DB_FILEPATH) {
     Remove-Item $DB_FILEPATH -Force
@@ -33,3 +37,5 @@ if (Test-Path $DB_FILEPATH) {
 } else {
     Write-Host "Nothing to clean."
 }
+
+Pop-Location
