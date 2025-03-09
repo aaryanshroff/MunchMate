6ccdace8f6037545a9e4a8339f211d85a27cb176
Kevin Nguyen <kc2nguye@uwaterloo.ca>
2025-03-09 19:43:53 -0400

    Add routes and/or buttons to register and login

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
 
 // TODO @djhoon24: Integrate currentUserId with the actual logged in userId (we should put this in local storage this when we login to a user)
 const currentUserId = 1;
@@ -6,20 +6,38 @@ const currentUserId = 1;
 const Navbar = () => {
     let navigate = useNavigate();
     return (
-        <div className='navbar bg-body-tertiary px-3 mb-3'>
-            <ul className='nav nav-pills'>
-                <div onClick={() => navigate('/')} className='btn'>
+        <div className="navbar bg-body-tertiary px-3 mb-3">
+            <ul className="nav nav-pills">
+                <div onClick={() => navigate("/")} className="btn">
                     MunchMate
                 </div>
-                <div onClick={() => navigate('/add-restaurant')} className='btn btn-secondary'>
+                <div
+                    onClick={() => navigate("/add-restaurant")}
+                    className="btn btn-secondary"
+                >
                     Add Restaurant
                 </div>
-                <div onClick={() => navigate(`/profile/${currentUserId}`)} className="btn btn-secondary">
+                <div
+                    onClick={() => navigate(`/profile/${currentUserId}`)}
+                    className="btn btn-secondary"
+                >
                     Profile
+                </div>
+                <div
+                    onClick={() => navigate("/register")}
+                    className="btn btn-secondary"
+                >
+                    Register
+                </div>
+                <div
+                    onClick={() => navigate("/login")}
+                    className="btn btn-secondary"
+                >
+                    Login
                 </div>
             </ul>
         </div>
     );
-}
+};
 
 export default Navbar;
