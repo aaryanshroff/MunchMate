c89f7deaa9c4dd898dbfa81e439ce081b4e598d8
Kevin Nguyen <kc2nguye@uwaterloo.ca>
2025-03-09 19:41:14 -0400

    Add functions to handle login and registration

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from hashlib import sha256
+import re
+from typing import Set
 
 import db
 from flask import Flask, request
@@ -7,7 +10,6 @@ from models.restaurant import Restaurant, RestaurantType
 
 app = Flask(__name__)
 db.init_app(app)
-
 
 # Enables CORS for all domains on all routes
 # Read more: https://github.com/corydolphin/flask-cors?tab=readme-ov-file#simple-usage
@@ -71,12 +73,15 @@ def add_restaurant():
         types = request.args.get("types", "").strip().split(",")
         images = request.args.get("images", "").strip().split(",")
 
-        results = _add_restaurant(name, address, city, state, zip_code, phone, types, images)
+        results = _add_restaurant(
+            name, address, city, state, zip_code, phone, types, images
+        )
 
         return {"data": results}, 200
     except Exception as e:
         print(f"{type(e).__name__}({e})")
         return {"error": str(e)}, 500
+
 
 def _add_restaurant(
     name: str = "",
@@ -86,7 +91,7 @@ def _add_restaurant(
     zip_code: str = "",
     phone: str = "",
     types: list[str] = [],
-    images: list[str] = []
+    images: list[str] = [],
 ) -> int:
     sql_file = Path("queries") / "add_restaurant.sql"
     add_restaurant_query = sql_file.read_text(encoding="utf-8")
@@ -95,7 +100,9 @@ def _add_restaurant(
     params = (name, address, city, state, zip_code, phone)
     query_response = db.query_db(add_restaurant_query, params)
 
-    restaurant_id = str( db.query_db("SELECT LAST_INSERT_ROWID()")[0]['LAST_INSERT_ROWID()'] )
+    restaurant_id = str(
+        db.query_db("SELECT LAST_INSERT_ROWID()")[0]["LAST_INSERT_ROWID()"]
+    )
 
     sql_file = Path("queries") / "assign_restaurant_types.sql"
     assign_types_query = sql_file.read_text(encoding="utf-8")
@@ -112,8 +119,9 @@ def _add_restaurant(
     for image_url in images:
         params = (restaurant_id, image_url)
         db.query_db(add_images_query, params)
-        
-    return query_response 
+
+    return query_response
+
 
 @app.get("/api/types")
 def get_types():
@@ -181,6 +189,90 @@ def get_user_reviews(uid):
         query = sql_file.read_text(encoding="utf-8")
         results = db.query_db(query, (uid,))
         return {"data": results}, 200
+    except Exception as e:
+        print(f"{type(e).__name__}({e})")
+        return {"error": str(e)}, 500
+
+
+# 5. Attempt login
+@app.post("/api/login")
+def login():
+    try:
+        username = request.form.get("username").strip()
+        password = request.form.get("password")
+        # No salt so that password is one-to-one with hash and we can pass it
+        # into the login query. Sufficiently secure for this project, otherwise use argon2-cffi
+        password_hash = sha256(password.encode()).hexdigest()
+
+        # Check if acct. is locked out
+        lockout_status = db.query_db_from_file(
+            Path("queries") / "get_account_lockout.sql", (username,)
+        )
+        if lockout_status[0]["locked_out"]:
+            return {
+                "error": "This account has been locked out. Please try again later."
+            }, 401
+
+        # Attempt login
+        results = db.query_db_from_file(
+            Path("queries") / "login.sql", (username, password_hash)
+        )
+        # Record login attempt, continue even if insert fails
+        try:
+            db.query_db_from_file(
+                Path("queries") / "add_login_attempt.sql", (username, bool(results))
+            )
+        except Exception as e:
+            print(f"{type(e).__name__}({e})")
+
+        if results:
+            return get_user_profile(results[0]["uid"])
+        else:
+            return {"error": "Invalid username or password"}, 401
+    except Exception as e:
+        print(f"{type(e).__name__}({e})")
+        return {"error": str(e)}, 500
+
+
+def check_password(pwd: str) -> Set[str]:
+    issues = set()
+    if len(pwd) < 6 or len(pwd) > 50:
+        issues.add("Password must be between 6 and 50 characters long.")
+    if not re.search(r"[a-z]", pwd):
+        issues.add("Password must contain at least one lowercase letter.")
+    if not re.search(r"[A-Z]", pwd):
+        issues.add("Password must contain at least one uppercase letter.")
+    if not re.search(r"\d", pwd):
+        issues.add("Password must contain at least one digit.")
+    if not re.search(r"[@$!%*#?&]", pwd):
+        issues.add("Password must contain at least one special character (@$!%*#?&).")
+
+    return issues
+
+
+# 6. Register new account
+@app.post("/api/register")
+def register():
+    try:
+        username = request.form.get("username").strip()
+        first_name = request.form.get("first_name").strip()
+        last_name = request.form.get("last_name").strip()
+        email = request.form.get("email").strip()
+        password = request.form.get("password")
+        password_hash = sha256(password.encode()).hexdigest()
+
+        password_issues = check_password(password)
+
+        if password_issues:
+            return {"error": list(password_issues)}, 400
+
+        db.query_db_from_file(
+            Path("queries") / "create_account.sql",
+            (username, first_name, last_name, email, password_hash),
+        )
+
+        return {}, 201
+
     except Exception as e:
         print(f"{type(e).__name__}({e})")
         return {"error": str(e)}, 500
