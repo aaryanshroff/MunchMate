3e1e22fa44b4c7c8eed91abbab0b25474c9efd04
Kevin Nguyen <kc2nguye@uwaterloo.ca>
2025-03-10 03:32:10 -0400

    Formatting and directory fix

--- scripts/powershell/cleanup_db.ps1
+++ scripts/powershell/cleanup_db.ps1
@@ -5,7 +5,9 @@ $ErrorActionPreference = "Stop"
 Push-Location
 
 # All paths relative to our backend directory
-Set-Location -Path "..\..\backend"
+
+# Move to directory containing script
+Set-Location -Path $PSScriptRoot
 
 # Environment Variables
 #
@@ -32,7 +34,8 @@ $DB_FILEPATH = ""
 if ($DB_TYPE -eq "sample") {
     Write-Host "Cleaning sample database..."
     $DB_FILEPATH = $SAMPLE_DB_FILEPATH
-} else {
+}
+else {
     Write-Host "Cleaning production database..."
     $DB_FILEPATH = $PROD_DB_FILEPATH
 }
@@ -48,7 +51,8 @@ Write-Host "Cleaning $DB_TYPE database..."
 if (Test-Path $DB_FILEPATH) {
     Remove-Item $DB_FILEPATH -Force
     Write-Host "Cleaned dataset!"
-} else {
+}
+else {
     Write-Host "Nothing to clean."
 }
 
